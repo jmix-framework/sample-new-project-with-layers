5b593e845292e5824ac7b959d7e0afe32edcb61d
Konstantin Krivopustov <krivopustov@haulmont.com>
2021-08-24 14:49:50 +0400

    Upgrade to Jmix 1.0.1

--- sample-data/src/main/java/com/company/sample/data/SampleDataConfiguration.java
+++ sample-data/src/main/java/com/company/sample/data/SampleDataConfiguration.java
@@ -2,6 +2,7 @@ package com.company.sample.data;
 
 import io.jmix.core.annotation.JmixModule;
 import io.jmix.data.DataConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.*;
@@ -16,8 +17,15 @@ public class SampleDataConfiguration {
 
     @Bean
     @Primary
-    @ConfigurationProperties(prefix = "main.datasource")
-    DataSource dataSource() {
-        return DataSourceBuilder.create().build();
+    @ConfigurationProperties("main.datasource")
+    DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("main.datasource.hikari")
+    DataSource dataSource(DataSourceProperties dataSourceProperties) {
+        return dataSourceProperties.initializeDataSourceBuilder().build();
     }
 }
---
+++ sample-ui/src/main/java/com/company/sample/ui/screen/main/MainScreen.java
@@ -0,0 +1,61 @@
+package com.company.sample.ui.screen.main;
+
+import com.company.sample.data.service.SampleService;
+import io.jmix.ui.Notifications;
+import io.jmix.ui.ScreenTools;
+import io.jmix.ui.component.AppWorkArea;
+import io.jmix.ui.component.Button;
+import io.jmix.ui.component.Window;
+import io.jmix.ui.component.mainwindow.Drawer;
+import io.jmix.ui.icon.JmixIcon;
+import io.jmix.ui.navigation.Route;
+import io.jmix.ui.screen.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@UiController("MainScreen")
+@UiDescriptor("main-screen.xml")
+@Route(path = "main", root = true)
+public class MainScreen extends Screen implements Window.HasWorkArea {
+
+    @Autowired
+    private ScreenTools screenTools;
+
+    @Autowired
+    private AppWorkArea workArea;
+    @Autowired
+    private Drawer drawer;
+    @Autowired
+    private Button collapseDrawerButton;
+    @Autowired
+    private Notifications notifications;
+    @Autowired
+    private SampleService sampleService;
+
+    @Override
+    public AppWorkArea getWorkArea() {
+        return workArea;
+    }
+
+    @Subscribe("collapseDrawerButton")
+    private void onCollapseDrawerButtonClick(Button.ClickEvent event) {
+        drawer.toggle();
+        if (drawer.isCollapsed()) {
+            collapseDrawerButton.setIconFromSet(JmixIcon.CHEVRON_RIGHT);
+        } else {
+            collapseDrawerButton.setIconFromSet(JmixIcon.CHEVRON_LEFT);
+        }
+    }
+
+    @Subscribe
+    public void onAfterShow(AfterShowEvent event) {
+        screenTools.openDefaultScreen(
+                UiControllerUtils.getScreenContext(this).getScreens());
+
+        screenTools.handleRedirect();
+    }
+
+    @Subscribe("sayHello")
+    public void onSayHelloClick(Button.ClickEvent event) {
+        notifications.create().withCaption(sampleService.echo("Hello")).show();
+    }
+}
