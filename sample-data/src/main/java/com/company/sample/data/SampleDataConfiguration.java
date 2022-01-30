2ab3070276156d74d12703a2486e67875083a629
Konstantin Krivopustov <krivopustov@haulmont.com>
2022-01-30 16:03:33 +0400

    Fix module dependencies

--- sample-data/src/main/java/com/company/sample/data/SampleDataConfiguration.java
+++ sample-data/src/main/java/com/company/sample/data/SampleDataConfiguration.java
@@ -1,18 +1,17 @@
 package com.company.sample.data;
 
 import io.jmix.core.annotation.JmixModule;
-import io.jmix.data.DataConfiguration;
+import io.jmix.eclipselink.EclipselinkConfiguration;
 import io.jmix.securitydata.SecurityDataConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.*;
 
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan
-@JmixModule(dependsOn = {SecurityDataConfiguration.class})
+@JmixModule(dependsOn = {EclipselinkConfiguration.class, SecurityDataConfiguration.class})
 @PropertySource(name = "com.company.sample.data", value = "classpath:/com/company/sample/data/module.properties")
 public class SampleDataConfiguration {
 
